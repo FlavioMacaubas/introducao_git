7d75a7141989d8c8b69781983adcb2e3e0ae3931
Flávio Macaúbas <flaviomacaubas@gmail.com>
2021-09-02 11:53:12 -0300

    Nova abordagem no programa edu.py

--- edu.py
+++ edu.py
@@ -1,14 +1,20 @@
 # Aplicativo - Aprovação e frequência
 
 # Informações do usuário 
-nota = float(input("Informe a nota final: \n").replace(',','.'))
 freq = float(input("Informe a frequência - em porcentagem: \n").replace(',','.'))
-reprovacoes = int(input("Informe quantidade de reprovações: \n").replace(',','.'))
-cra = float(input("Informe o CRA: \n").replace(',','.'))
 
-# Teste condições
-aprovado = True if ((nota >= 7) and (freq >= 75)) else False
-desligado = True if (reprovacoes >= 2) or (cra < 5) else False
-
-print(f"O aluno foi aprovado: {aprovado} \nO aluno foi desligado: {desligado}")
+# Verificando reprovação
+if freq < 75:
+    situacao = 'reprovado por falta'
+else:
+    nota = float(input("Informe a nota: \n").replace(',','.'))
+    if nota >= 7:
+        situacao = "aprovado por média"
+    elif (4 <= nota < 7):
+        nota_final = float(input("Informe a nota final/recuperação: \n"))
+        situacao = "aprovado na final" if (0.6*nota + 0.4*nota_final) >= 5 else "reprovado na final"
+    else:
+        situacao = "reprovado por média"
+        
+print(f"O aluno foi {situacao}")
 
