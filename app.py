52bb21ce8f381038f7d4319967eb16765522c067
Flávio Macaúbas <flaviomacaubas@gmail.com>
2021-09-09 11:20:04 -0300

    Atualiza  o aplicativo app.py"
    git status

--- app.py
+++ app.py
@@ -7,4 +7,17 @@ altura = float( input("Informe sua altura: \n").replace(',','.') )
 # Calcular IMC
 imc = peso / altura**2
 
-print(f"Olá {nome}, seu peso é de {peso}kg e altura de {altura}m, com IMC de {imc:.2f}.")
+if imc < 18.5:
+    situacao = 'abaixo do peso'
+elif 18.5 <= imc < 25:
+    situacao = 'peso normal'
+elif 25 <= imc < 30:
+    situacao = 'sobrepeso'
+elif 30 <= imc < 35:
+    situacao = 'obesidade grau 1'
+elif 35 <= imc < 40:
+    situacao = 'obesidade grau 2'
+else:
+    situacao = 'obesidade grau 3 ou mórbida'
+
+print(f"Sua altura é de {altura:.2f} e peso de {peso:.2f}, seu IMC é de {imc:.2f} classificada como {situacao}.")
