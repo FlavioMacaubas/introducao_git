caab2857fc05af9bd7d2585ce40f1426838d6d48
Flávio Macaúbas <flaviomacaubas@gmail.com>
2021-09-23 12:09:46 -0300

    Adicionar arquivos edu_medio.py e cashier.py

---
+++ cashier.py
@@ -0,0 +1,30 @@
+# Programa para caixa de supermercado
+
+# Lista de despesas
+items = []
+
+while True:
+    price = input("Informe o preço do produto ou digite q para encerrar: ")
+
+    if price.lower().strip() == 'q':
+        print('Obrigado')
+        break
+
+    qtd = input("informe a quantidade para sair ou digite q para encerrar: ")
+
+    if qtd.lower().strip() == 'q':
+        print('Obrigado')
+        break
+
+    value = float(price)*float(qtd)
+    items.append(value)
+
+
+if len(items) > 0: 
+    print(f'Valor da compra: {sum(items):.2f}')  
+    cash = float(input("Qual valor da nota?"))
+    print(f"O troco é de: {(cash - sum(items) ):.2f}")
+else:
+    print('Obrigado')
+
+
